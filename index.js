8561bb5cf172e463e83dc4d6043c6bf98f12c24c
scripshub <lucasjoaquim478@gmail.com>
2026-05-18 16:36:08 -0300

    TESTE3: ESM index.js, startCommand 'node index.js'

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 import http from "http";
-const s = http.createServer((q, r) => r.end("INDEX_JS_WORKS"));
-s.listen(3000, () => console.log("[INDEX] SERVER ON 3000 - PID:", process.pid));
+const s = http.createServer((q, r) => r.end("ESM_WORKS"));
+s.listen(3000, () => console.log("[ESM] SERVER ON 3000 - PID:", process.pid));
